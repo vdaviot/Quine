bedc70c73a48faecc1109d91707ddfb50b6b1d47
Valentin DAVIOT <vdaviot@e1r10p4.42.fr>
2018-04-25 14:55:56 +0200

    Removed duplicated dir

---
+++ Colleen/bonus/Colleen.py
@@ -0,0 +1,3 @@
+# Pokemon!
+def getPokemon(): return '# Pokemon!\ndef getPokemon(): return %r\nif __name__ == "__main__": a = getPokemon(); print(a %% a) # Catch em all!'
+if __name__ == "__main__": a = getPokemon(); print(a % a) # Catch em all!
