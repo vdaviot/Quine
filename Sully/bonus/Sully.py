372fb1efb8588b6692d6a92a1a4468f17aa77a3d
Valentin DAVIOT <vdaviot@e1r10p4.42.fr>
2018-04-25 14:54:59 +0200

    Finished bonuses, now asm..

---
+++ Sully/bonus/Sully.py
@@ -0,0 +1,5 @@
+import os
+nb = 5 if (__file__ != "Sully.py") else 6
+if nb == 0: exit()
+s = 'import os\nnb = %d if (__file__ != "Sully.py") else 6\nif nb == 0: exit()\ns = %r\nwith open("Sully_%%d.py" %% (nb - 1), "w") as fd: fd.write(s %% (nb - 1, s)); fd.close(); os.system("python Sully_%%d.py" %% (nb - 1)) if nb > 0 else None'
+with open("Sully_%d.py" % (nb - 1), "w") as fd: fd.write(s % (nb - 1, s)); fd.close(); os.system("python Sully_%d.py" % (nb - 1)) if nb > 0 else None
