67af54f203ef2525fe5fadc231e2a572142ad8c9
Valentin DAVIOT <vdaviot@e1r11p3.42.fr>
2018-05-08 13:27:26 +0200

    progression, segfault on printf

--- Sully/ASM/Sully.asm
+++ Sully/ASM/Sully.asm
@@ -8,11 +8,19 @@ SECTION .text
 	extern _system
 	default rel
 
+%macro PRINTF 1
+lea rdi, [%1]
+mov rsi, r12
+mov rdx, 10
+call _printf
+%endmacro
+
 %macro FPRINTF 0
-mov rdi, [Fd] ; fd
+; mov rdi, Fd ; fd
+mov rdi, rax
 lea rsi, [Quine] ; quine
 mov rdx, r12 ; va_args
-mov rcx, 10
+mov rcx, 10 ; \n
 call _fprintf
 %endmacro
 
@@ -27,11 +35,11 @@ call _sprintf
 lea rdi, [File]
 lea rsi, [Mode]
 call _fopen
-mov [Fd], rax
+;mov [Fd], rax
 %endmacro
 
 %macro FCLOSE 0
-lea rdi, [Fd]
+mov rdi, rax
 call _fclose
 %endmacro
 
@@ -72,30 +80,29 @@ call _system
 
 
 _main:
-	sub rsp, 16
 	mov r12, 5
 	push r12
 	SPRINTF ; ecriture nom fichier
 	FOPEN ; ouverture fichier
-	FPRINTF ; ecriture Quine
+	; FPRINTF ; ecriture Quine ; pb avec fd -> fprintf
+	; PRINTF Quine; DEBUG
 	FCLOSE ; fermeture fichier
 	OBJ ; link obj
 	FORM ; formatting .o
 	COMPIL ; exec
-	add rsp, 8
+	pop r12
 	mov rax, 0
-	leave
 	ret
 
 SECTION .data
 
-Quine: db ">Sully_%1$d.asm%2$c", 10, 0
+Quine: db ">Sully_%1$d.asm%2$c", 0
 Mode: db "w", 0
 Nasm: db "/Users/vdaviot/homebrew/bin/nasm -f macho64 %s", 0
-Compil: db "clang -Wall -Wextra -Werror %s -o %s", 0
+Compil: db "clang -Wall -Wextra -Werror %s.o -o %s", 0
 Execute: db "./%s", 0
 Filename: db "Sully_%d.asm", 0
-Objectname: db "Sully_%d.o", 0
+Objectname: db "Sully_%d", 0
 Object: times 512 db 0
 File: times 512 db 0
 Command: times 512 db 0
