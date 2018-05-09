9a8609212ea8967f9a7bf97b0b55863792543dd7
Valentin DAVIOT <vdaviot@e1r10p3.42.fr>
2018-05-09 14:45:17 +0200

    Fonctionnel ?

--- Sully/ASM/Sully.asm
+++ Sully/ASM/Sully.asm
@@ -78,6 +78,7 @@ call _system
 _main:
 mov r12, 5
 cmp r12, 0
+jl end
 push r12
 SPRINTF
 FOPEN
@@ -90,11 +91,11 @@ FORM
 COMPIL
 EXEC
 pop r12
+end:
 mov rax, 0
 ret
 SECTION .data
-Fdp: db "", 10, 0
-Quine: db "SECTION .text%2$cglobal _main%2$cextern _sprintf%2$cextern _fprintf%2$cextern _printf%2$cextern _fopen%2$cextern _fclose%2$cextern _system%2$cdefault rel%2$c%%macro PRINTF 1%2$clea rdi, [%%1]%2$cmov rsi, r12%2$cmov rdx, 10%2$cmov rcx, 34%2$clea r8, [%%1]%2$ccall _printf%2$c%%endmacro%2$c%%macro FPRINTF 0%2$cmov rdi, [Fd]%2$clea rsi, [Quine]%2$cmov rdx, r12%2$cmov rcx, 10%2$cmov r8, 34%2$clea r9, [Quine]%2$ccall _fprintf%2$c%%endmacro%2$c%%macro SPRINTF 0%2$clea rdi, [File]%2$clea rsi, [Filename]%2$cmov rdx, r12%2$ccall _sprintf%2$c%%endmacro%2$c%%macro FOPEN 0%2$clea rdi, [File]%2$clea rsi, [Mode]%2$ccall _fopen%2$cmov qword [Fd], rax%2$c%%endmacro%2$c%%macro FCLOSE 0%2$cmov rdi, Fd%2$ccall _fclose%2$c%%endmacro%2$c%%macro COMPIL 0%2$clea rdi, [Command]%2$clea rsi, [Compil]%2$clea rdx, [Object]%2$clea rcx, [Object]%2$ccall _sprintf%2$clea rdi, [Command]%2$ccall _system%2$c%%endmacro%2$c%%macro FORM 0%2$clea rdi, [Object]%2$clea rsi, [Objectname]%2$cmov rdx, r12%2$ccall _sprintf%2$c%%endmacro%2$c%%macro OBJ 0%2$clea rdi, [Command]%2$clea rsi, [Nasm]%2$clea rdx, [File]%2$ccall _sprintf %2$clea rdi, [Command]%2$ccall _system%2$c%%endmacro%2$c%%macro EXEC 0%2$clea rdi, [Command]%2$clea rsi, [Execute]%2$clea rdx, [Objectname]%2$cmov rcx, r12%2$ccall _sprintf%2$clea rdi, [Execute]%2$ccall _system%2$c%%endmacro%2$c_main:%2$cmov r12, %1$d%2$ccmp r12, 0%2$cjl rip%2$cpush r12%2$cSPRINTF%2$cFOPEN%2$csub r12, 1%2$cFPRINTF%2$cadd r12, 1%2$cFCLOSE%2$cOBJ%2$cFORM%2$cCOMPIL%2$cpop r12%2$cjmp rip%2$crip:%2$cmov rax, 0%2$cret%2$cSECTION .data%2$cQuine: db %3$c%s%3$c, 0%2$cMode: db %3$cw%3$c, 0%2$cNasm: db %3$c/Users/vdaviot/homebrew/bin/nasm -f macho64 %%s%3$c, 0%2$cCompil: db %3$cclang -Wall -Wextra -Werror %%s.o -o %%s%3$c, 0%2$cExecute: db %3$c./%%s%3$c, 0%2$cFilename: db %3$cSully_%%d.asm%3$c, 0%2$cObjectname: db %3$cSully_%%d%3$c, 0%2$cObject: times 512 db 0%2$cFile: times 512 db 0%2$cCommand: times 512 db 0%2$cSECTION .bss%2$cFd: resb 1%2$c", 0
+Quine: db "SECTION .text%2$cglobal _main%2$cextern _sprintf%2$cextern _fprintf%2$cextern _printf%2$cextern _fopen%2$cextern _fclose%2$cextern _system%2$cdefault rel%2$c%%macro PRINTF 1%2$clea rdi, [%%1]%2$cmov rsi, r12%2$cmov rdx, 10%2$cmov rcx, 34%2$clea r8, [%%1]%2$ccall _printf%2$c%%endmacro%2$c%%macro FPRINTF 0%2$cmov rdi, [Fd]%2$clea rsi, [Quine]%2$cmov rdx, r12%2$cmov rcx, 10%2$cmov r8, 34%2$clea r9, [Quine]%2$ccall _fprintf%2$c%%endmacro%2$c%%macro SPRINTF 0%2$clea rdi, [File]%2$clea rsi, [Filename]%2$cmov rdx, r12%2$ccall _sprintf%2$c%%endmacro%2$c%%macro FOPEN 0%2$clea rdi, [File]%2$clea rsi, [Mode]%2$ccall _fopen%2$cmov qword [Fd], rax%2$c%%endmacro%2$c%%macro FCLOSE 0%2$cmov rdi, [Fd]%2$ccall _fclose%2$c%%endmacro%2$c%%macro COMPIL 0%2$clea rdi, [Command]%2$clea rsi, [Compil]%2$clea rdx, [Object]%2$clea rcx, [Object]%2$ccall _sprintf%2$clea rdi, [Command]%2$ccall _system%2$c%%endmacro%2$c%%macro FORM 0%2$clea rdi, [Object]%2$clea rsi, [Objectname]%2$cmov rdx, r12%2$ccall _sprintf%2$c%%endmacro%2$c%%macro OBJ 0%2$clea rdi, [Command]%2$clea rsi, [Nasm]%2$clea rdx, [File]%2$ccall _sprintf%2$clea rdi, [Command]%2$ccall _system%2$c%%endmacro%2$c%%macro EXEC 0%2$clea rdi, [File]%2$clea rsi, [Objectname]%2$cmov rdx, r12%2$ccall _sprintf%2$clea rdi, [Command]%2$clea rsi, [Execute]%2$clea rdx, [File]%2$ccall _sprintf%2$clea rdi, [Command]%2$ccall _system%2$c%%endmacro%2$c_main:%2$cmov r12, %1$d%2$ccmp r12, 0%2$cjl end%2$cpush r12%2$cSPRINTF%2$cFOPEN%2$csub r12, 1%2$cFPRINTF%2$cadd r12, 1%2$cFCLOSE%2$cOBJ%2$cFORM%2$cCOMPIL%2$cEXEC%2$cpop r12%2$cend:%2$cmov rax, 0%2$cret%2$cSECTION .data%2$cQuine: db %3$c%s%3$c, 0%2$cMode: db %3$cw%3$c, 0%2$cNasm: db %3$c/Users/vdaviot/homebrew/bin/nasm -f macho64 %%s%3$c, 0%2$cCompil: db %3$cclang -Wall -Wextra -Werror %%s.o -o %%s%3$c, 0%2$cExecute: db %3$c./%%s%3$c, 0%2$cFilename: db %3$cSully_%%d.asm%3$c, 0%2$cObjectname: db %3$cSully_%%d%3$c, 0%2$cObject: times 512 db 0%2$cFile: times 512 db 0%2$cCommand: times 512 db 0%2$cSECTION .bss%2$cFd: resq 1%2$c", 0
 Mode: db "w", 0
 Nasm: db "/Users/vdaviot/homebrew/bin/nasm -f macho64 %s", 0
 Compil: db "clang -Wall -Wextra -Werror %s.o -o %s", 0
@@ -105,4 +106,4 @@ Object: times 512 db 0
 File: times 512 db 0
 Command: times 512 db 0
 SECTION .bss
-Fd: resb 1
+Fd: resq 1
