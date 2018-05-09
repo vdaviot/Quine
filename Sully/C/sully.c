eb90fc766a0cedeafec761b3bd8b35fc94a34780
Valentin DAVIOT <vdaviot@e1r10p3.42.fr>
2018-05-09 15:09:49 +0200

    Verif Sully C

--- Sully/C/sully.c
+++ Sully/C/sully.c
@@ -11,13 +11,13 @@ int main(){
 	char * filename;
 	FILE * fd;
 	char * command;
-	char content[] = "#include <stdio.h>%c#include <string.h>%c#include <stdlib.h>%c%cint main(){%c%cint nb = %d;%c%cif (strcmp(__FILE__, %cSully.c%c) == 0)%c%c%cnb = nb + 1;%c%cif (nb <= 0)%c%c%creturn 0;%c%cchar * filename;%c%cFILE * fd;%c%cchar * command;%c%cchar content[] = %c%s%c;%c%c%casprintf(&filename, %cSully_%%d.c%c, nb - 1);%c%cfd = fopen(filename, %cw%c);%c%cfprintf(fd, content, 10, 10, 10, 10, 10, 9, nb - 1, 10, 9, 34, 34, 10, 9, 9, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, content, 34, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 34, 34, 10, 9, 10, 9, 9, 10, 9, 10);%c%cfclose(fd);%c%casprintf(&command, %cgcc -Wall -Wextra -Wall %%s -o Sully && ./Sully%c, filename);%c%cif (nb >= 1)%c%c%csystem(command);%c%creturn 1;%c}";
+	char content[] = "#include <stdio.h>%c#include <string.h>%c#include <stdlib.h>%c%cint main(){%c%cint nb = %d;%c%cif (strcmp(__FILE__, %cSully.c%c) == 0)%c%c%cnb = nb + 1;%c%cif (nb <= 0)%c%c%creturn 0;%c%cchar * filename;%c%cFILE * fd;%c%cchar * command;%c%cchar content[] = %c%s%c;%c%c%casprintf(&filename, %cSully_%%d.c%c, nb - 1);%c%cfd = fopen(filename, %cw%c);%c%cfprintf(fd, content, 10, 10, 10, 10, 10, 9, nb - 1, 10, 9, 34, 34, 10, 9, 9, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, content, 34, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 34, 34, 10, 9, 10, 9, 9, 10, 9, 10);%c%cfclose(fd);%c%casprintf(&command, %cgcc -Wall -Wextra -Wall %%s -o Sully_%%d && ./Sully_%%d%c, filename, nb - 1, nb - 1);%c%cif (nb >= 1)%c%c%csystem(command);%c%creturn 1;%c}";
 
 	asprintf(&filename, "Sully_%d.c", nb - 1);
 	fd = fopen(filename, "w");
 	fprintf(fd, content, 10, 10, 10, 10, 10, 9, nb - 1, 10, 9, 34, 34, 10, 9, 9, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, content, 34, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 34, 34, 10, 9, 10, 9, 9, 10, 9, 10);
 	fclose(fd);
-	asprintf(&command, "gcc -Wall -Wextra -Wall %s -o Sully && ./Sully", filename);
+	asprintf(&command, "gcc -Wall -Wextra -Wall %s -o Sully_%d && ./Sully_%d", filename, nb - 1, nb - 1);
 	if (nb >= 1)
 		system(command);
 	return 1;
