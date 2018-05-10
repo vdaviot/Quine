c73140ea6feb06dfb440be529cb345fc21927b8b
Valentin DAVIOT <vdaviot@e1r10p4.42.fr>
2018-05-10 17:23:56 +0200

    YOLOOOO

--- Sully/bonus/Sully.py
+++ Sully/bonus/Sully.py
@@ -1,5 +1,5 @@
 import os
 nb = 5 if (__file__ != "Sully.py") else 6
 if nb == 0: exit()
-s = 'import os\nnb = %d if (__file__ != "Sully.py") else 6\nif nb == 0: exit()\ns = %r\nwith open("Sully_%%d.py" %% (nb - 1), "w") as fd: fd.write(s %% (nb - 1, s)); fd.close(); os.system("python Sully_%%d.py" %% (nb - 1)) if nb > 0 else None'
-with open("Sully_%d.py" % (nb - 1), "w") as fd: fd.write(s % (nb - 1, s)); fd.close(); os.system("python Sully_%d.py" % (nb - 1)) if nb > 0 else None
+s = 'import os\nnb = %d if (__file__ != "Sully.py") else %d\nif nb == 0: exit()\ns = %r\nwith open("Sully_%%d.py" %% (nb - 1), "w") as fd: fd.write(s %% (nb - 1, nb, s)); fd.close(); os.system("python Sully_%%d.py" %% (nb - 1)) if nb > 0 else None'
+with open("Sully_%d.py" % (nb - 1), "w") as fd: fd.write(s % (nb - 1, nb, s)); fd.close(); os.system("python Sully_%d.py" % (nb - 1)) if nb > 0 else None
